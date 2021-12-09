b7d48263331e3c471d968a0be94a33d457aa192e
Alejandro F <afuste7@gmail.com>
2021-12-09 16:59:50 -0500

    changed dictionary property in manager

--- entities/manager.py
+++ entities/manager.py
@@ -1,7 +1,7 @@
-class CompanyUser:
-    def __init__(self, employee_id: int, first_name: str, last_name: str, company_role: str, user_name: str,
+class Manager:
+    def __init__(self, manager_id: int, first_name: str, last_name: str, company_role: str, user_name: str,
                  user_password: str):
-        self.employee_id = employee_id
+        self.manager_id = manager_id
         self.first_name = first_name
         self.last_name = last_name
         self.company_role = company_role
@@ -9,16 +9,15 @@ class CompanyUser:
         self.user_password = user_password
 
     def __str__(self):
-        return f'user id:{self.employee_id}, first name: {self.first_name}, last name: {self.last_name}, ' \
+        return f'manager id:{self.manager_id}, first name: {self.first_name}, last name: {self.last_name}, ' \
                f'company role: {self.company_role}, user name: {self.user_name}, password: {self.user_password}'
 
     def make_company_user_dictionary(self):
         return {
-            "userId": self.employee_id,
+            "managerId": self.manager_id,
             "firstName": self.first_name,
             "lastName": self.last_name,
             "companyRole": self.company_role,
             "userName": self.user_name,
             "userPassword": self.user_password
         }
-
