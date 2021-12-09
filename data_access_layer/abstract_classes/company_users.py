3c6b32dfb6d1e73944e9bc5a8e20e502452d6f7a
Alejandro F <afuste7@gmail.com>
2021-12-09 16:48:18 -0500

    added updated code for schema in schema file

---
+++ data_access_layer/abstract_classes/company_users.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+from entities.company_users import CompanyUser
+
+
+class CompanyUserDAO(ABC):
+    pass
--- utils/SCHEMA.sql
+++ utils/SCHEMA.sql
@@ -1,7 +1,7 @@
-drop table if exists company_user, reimbursement;
+drop table if exists employee, manager, reimbursement;
 
-create table company_user(
-	user_id serial not null primary key,
+create table employee(
+	employee_id serial not null primary key,
 	first_name varchar (30) not null,
 	last_name varchar (30) not null,
 	company_role varchar (30) not null,
@@ -10,16 +10,25 @@ create table company_user(
 
 );
 
+create table manager(
+	manager_id serial not null primary key,
+	first_name varchar (30) not null,
+	last_name varchar (30) not null,
+	company_role varchar (30) not null,
+	user_name varchar (30) not null,
+	user_password varchar (30) not null
+)
+
 
 create table reimbursement(
 	reimbursement_id serial not null primary key,
-	expense_name varchar (30) not null,
-	expense_amount decimal not null,
-	expense_reason varchar (30) not null,
-	expense_date date not null,
+	reimbursement_amount decimal not null,
+	reimbursement_reason text not null,
+	reimbursement_date date not null,
 	status varchar (30) not null,
-	reject_reason text,
 	accepted_date date,
-	user_id int references company_user(user_id) on delete cascade
+	reason text,
+	employee_id int references employee(employee_id) on delete cascade,
+	manager_id int references manager(manager_id) on delete cascade
 
 );
