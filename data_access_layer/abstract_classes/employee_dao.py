ec4c6e7186d69e79332ed00abca2820ea4cb0088
Alejandro F <afuste7@gmail.com>
2021-12-09 17:46:26 -0500

    added abstract method for employee and reimbursement

--- data_access_layer/abstract_classes/employee_dao.py
+++ data_access_layer/abstract_classes/employee_dao.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-
+from typing import List
 from entities.employee import Employee
 
 
 class EmployeeDAO(ABC):
     @abstractmethod
-    def create_new_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
+    def get_all_reimbursements(self) -> List[Employee]:
         pass
