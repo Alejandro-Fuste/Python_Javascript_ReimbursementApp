ad9006cf8ef44b81e33a2c98c3d2a85ade3c16e9
Alejandro F <afuste7@gmail.com>
2021-12-09 16:49:05 -0500

    added updated code for seed file

--- utils/seed.sql
+++ utils/seed.sql
@@ -1,9 +1,8 @@
--- Create users
-insert into company_user values (default, 'Luke', 'Skywalker', 'Manager', 'Master Jedi', 'TheRealChosenOne');
-insert into company_user values (default, 'Leia', 'Organa', 'Manager', 'Princess Leia', 'PrincessLeiaJedi');
-insert into company_user values (default, 'Rey', 'Skywalker', 'Employee', 'NinjaCatGirl', 'JakkuJedi1');
+-- Create employees
+insert into employee values (default, 'Rey', 'Skywalker', 'Employee', 'NinjaCatGirl', 'JakkuJedi1');
+
+-- Create managers
+insert into manager values (default, 'Luke', 'Skywalker', 'Manager', 'Master Jedi', 'TheRealChosenOne');
 
 --Create reimbursements
-
-insert into reimbursement values (default, 'Leia', 'Organa', 20.00, 'Travel gas', '12-9-2021', 'pending', null, null, 1)
-insert into reimbursement values (default, 'Luke', 'Skywalker', 100.00, 'Food', '12-8-2021', 'pending', null, null, 3)
+insert into reimbursement values (default, 20.00, 'Travel gas', '12-9-2021', 'pending', null, null, 1, 1);
