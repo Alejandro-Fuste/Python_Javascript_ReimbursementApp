964acf8cec18b9bf512c8f1e992dc23c0678d6b4
Alejandro F <afuste7@gmail.com>
2021-12-09 17:12:48 -0500

    changed code in reimbursement

--- entities/reimbursements.py
+++ entities/reimbursements.py
@@ -2,35 +2,33 @@ from datetime import datetime
 
 
 class Reimbursements:
-    def __init__(self, reimbursement_id: int, manager_first_name: str, manager_last_name: str,
-                 reimbursement_amount: float, reimbursement_reason: str, reimbursement_date: datetime, status: str,
-                 reject_reason: str, accepted_date: datetime, user_id: int):
+    def __init__(self, reimbursement_id: int, reimbursement_amount: float, reimbursement_reason: str,
+                 reimbursement_date: datetime, status: str, reject_reason: str, accepted_date: datetime,
+                 employee_id: int, manager_id: int):
         self.reimbursement_id = reimbursement_id
-        self.manager_first_name = manager_first_name
-        self.manager_last_name = manager_last_name
         self.reimbursement_amount = reimbursement_amount
         self.reimbursement_reason = reimbursement_reason
         self.reimbursement_date = reimbursement_date
         self.status = status
-        self.reject_reason = reject_reason
         self.accepted_date = accepted_date
-        self.user_id = user_id
+        self.reject_reason = reject_reason
+        self.employee_id = employee_id
+        self.manager_id = manager_id
 
     def __str__(self):
-        return f'reimbursement id: {self.reimbursement_id},\nmanager first name: {self.manager_first_name},\n' \
-               f'manager last name: {self.manager_last_name},\nreimbursement amount: {self.reimbursement_amount},\n' \
-               f'reimbursement reason: {self.reimbursement_reason}, status: {self.status},\n' \
-               f'rejectReason: {self.reject_reason},\naccepted date: {self.accepted_date},\nuser ID: {self.user_id}'
+        return f'reimbursement id: {self.reimbursement_id},\nreimbursement amount: {self.reimbursement_amount},\n' \
+               f'reimbursement reason:{self.reimbursement_reason},\nstatus: {self.status},\nrejectReason: ' \
+               f'{self.reject_reason},\naccepted date: {self.accepted_date},\nemployee ID: {self.employee_id},\n' \
+               f'manager ID: {self.manager_id}'
 
     def make_reimbursement_dictionary(self):
         return {
             "reimbursementId": self.reimbursement_id,
-            "managerFirstName": self.manager_first_name,
-            "managerLastName": self.manager_last_name,
             "reimbursementAmount": self.reimbursement_amount,
             "reimbursementReason": self.reimbursement_reason,
             "status": self.status,
             "rejectReason": self.reject_reason,
             "acceptedDate": self.accepted_date,
-            "userID": self.user_id
+            "employeeID": self.employee_id,
+            "managerID": self.manager_id
         }
