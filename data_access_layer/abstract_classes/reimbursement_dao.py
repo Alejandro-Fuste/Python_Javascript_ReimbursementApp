e84f80cd96569692423590f983bd54aa9e6d635b
Alejandro F <afuste7@gmail.com>
2021-12-10 21:12:29 -0500

    added more abstract methods in reimbursements and added category to reimbursement entity

--- data_access_layer/abstract_classes/reimbursement_dao.py
+++ data_access_layer/abstract_classes/reimbursement_dao.py
@@ -5,12 +5,45 @@ from entities.reimbursement import Reimbursement
 
 
 class ReimbursementDAO(ABC):
+    # for employees to create new request
     @abstractmethod
     def create_new_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
         pass
 
+    # so manager can view approved, denied, and pending reimbursements
+    @abstractmethod
     def get_all_reimbursement_requests(self) -> List[Reimbursement]:
         pass
 
+    # to view all the employees reimbursements
+    @abstractmethod
+    def get_reimbursements_by_employee_id(self) -> List[Reimbursement]:
+        pass
+
+    # for manager to approve/deny and leave comments
+    @abstractmethod
     def update_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
         pass
+
+    # get statistics
+    @abstractmethod
+    def get_total_reimbursements_amount(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_total_reimbursements_amount_by_employee(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_total_reimbursements_amount_by_month(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_total_reimbursements_amount_by_category(self) -> float:
+        pass
+
+    @abstractmethod
+    def get_top_five_highest_amounts(self) -> List[Reimbursement]:
+        pass
+
+
--- entities/reimbursement.py
+++ entities/reimbursement.py
@@ -1,7 +1,7 @@
 class Reimbursement:
     def __init__(self, reimbursement_id: int, reimbursement_amount: float, reimbursement_reason: str,
-                 reimbursement_date: str, status: str, reject_reason: str, accepted_date: str,
-                 employee_id: int, manager_id: int):
+                 reimbursement_date: str, status: str, reject_reason: str, accepted_date: str, category: str,
+                 employee_name: str, manager_name: str):
         self.reimbursement_id = reimbursement_id
         self.reimbursement_amount = reimbursement_amount
         self.reimbursement_reason = reimbursement_reason
@@ -9,14 +9,15 @@ class Reimbursement:
         self.status = status
         self.accepted_date = accepted_date
         self.reject_reason = reject_reason
-        self.employee_id = employee_id
-        self.manager_id = manager_id
+        self.category = category
+        self.employee_name = employee_name
+        self.manager_name = manager_name
 
     def __str__(self):
         return f'reimbursement id: {self.reimbursement_id},\nreimbursement amount: {self.reimbursement_amount},\n' \
                f'reimbursement reason:{self.reimbursement_reason},\nstatus: {self.status},\nrejectReason: ' \
-               f'{self.reject_reason},\naccepted date: {self.accepted_date},\nemployee ID: {self.employee_id},\n' \
-               f'manager ID: {self.manager_id}'
+               f'{self.reject_reason},\naccepted date: {self.accepted_date},\ncategory: {self.category},\n' \
+               f'employee ID: {self.employee_name},\nmanager ID: {self.manager_name}'
 
     def make_reimbursement_dictionary(self):
         return {
@@ -26,8 +27,9 @@ class Reimbursement:
             "status": self.status,
             "rejectReason": self.reject_reason,
             "acceptedDate": self.accepted_date,
-            "employeeID": self.employee_id,
-            "managerID": self.manager_id
+            "category": self.category,
+            "employeeName": self.employee_name,
+            "managerName": self.manager_name
         }
 
 #
