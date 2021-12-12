d163411906449de809a3e3058b9698252b8fa547
Alejandro F <afuste7@gmail.com>
2021-12-11 23:46:04 -0500

    added the rest of the tests for the reimbursement dao

--- tests_for_app/postgres_tests/dao_tests/test_reimbursement_dao.py
+++ tests_for_app/postgres_tests/dao_tests/test_reimbursement_dao.py
@@ -6,41 +6,49 @@ reimbursement: Reimbursement = Reimbursement(0, 20.00, 'Gas', 'Travel gas', '12-
                                              'null', 'Rey', 'Skywalker', 'Luke', 'Skywalker')
 
 updated_reimbursement = Reimbursement(0, 100.00, 'Gas', 'Travel gas', '12-9-2021', 'pending', 'null',
-                                             'null', 'Rey', 'Skywalker', 'Luke', 'Skywalker')
+                                      'null', 'Rey', 'Skywalker', 'Luke', 'Skywalker')
+
 
 def test_create_new_reimbursement_request():
     create_reimbursement = reimbursement_dao.create_new_reimbursement_request(reimbursement)
     assert create_reimbursement.reimbursement_id != 0
 
+
 def test_get_all_reimbursement_requests():
     reimbursements = reimbursement_dao.get_all_reimbursement_requests()
     assert len(reimbursements) > 2
+
 
 def test_get_reimbursements_by_employee_id():
     employee_reimbursements = reimbursement_dao.get_reimbursements_by_employee_id(1)
     assert len(employee_reimbursements) > 1
 
+
 def test_update_reimbursement_request():
     updated_request = reimbursement_dao.update_reimbursement_request(updated_reimbursement)
     assert updated_request.employee_last_name == updated_reimbursement.employee_last_name
 
-def test_
 
-    assert
+def test_get_total_reimbursements_amount():
+    total = reimbursement_dao.get_total_reimbursements_amount()
+    assert total > 0
 
-def test_
 
-    assert
+def test_get_total_reimbursements_amount_by_employee():
+    employee_total = reimbursement_dao.get_total_reimbursements_amount_by_employee()
+    assert employee_total > 0
 
-def test_
 
-    assert
+def test_get_total_reimbursements_amount_by_month():
+    monthly_total = reimbursement_dao.get_total_reimbursements_amount_by_month()
+    assert monthly_total > 0
 
-def test_
 
-    assert
+def test_get_total_reimbursements_amount_by_category():
+    category_total = reimbursement_dao.get_total_reimbursements_amount_by_category()
+    assert category_total > 0
 
-def test_
 
-    assert
-
+def test_get_top_five_highest_amounts():
+    amounts = reimbursement_dao.get_top_five_highest_amounts()
+    assert len(amounts) > 4
