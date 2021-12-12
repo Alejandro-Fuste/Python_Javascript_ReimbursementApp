50c292be4ae298bfe1de9ab57e01af2207157082
Alejandro F <afuste7@gmail.com>
2021-12-11 22:09:11 -0500

    added code for employee dao imp

---
+++ data_access_layer/implementation_classes/employee_postgres_dao_imp.py
@@ -0,0 +1,8 @@
+from data_access_layer.abstract_classes.employee_dao import EmployeeDAO
+from entities.employee import Employee
+from utils.database_connection import connection
+
+
+class EmployeePostgresDAO(EmployeeDAO):
+    def get_employee_by_username(self) -> Employee:
+        pass
