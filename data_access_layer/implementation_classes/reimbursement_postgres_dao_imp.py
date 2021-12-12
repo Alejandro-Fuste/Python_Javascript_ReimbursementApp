2f5721d8f43910d20bffce4818ed9c166bf47200
Alejandro F <afuste7@gmail.com>
2021-12-11 22:56:09 -0500

    added reimbursement methods

---
+++ data_access_layer/implementation_classes/reimbursement_postgres_dao_imp.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from data_access_layer.abstract_classes.reimbursement_dao import ReimbursementDAO
+from entities.reimbursement import Reimbursement
+from utils.database_connection import connection
+
+
+class ReimbursementPostgresDAO(ReimbursementDAO):
+    def create_new_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
+        pass
+
+    def get_all_reimbursement_requests(self) -> List[Reimbursement]:
+        pass
+
+    def get_reimbursements_by_employee_id(self) -> List[Reimbursement]:
+        pass
+
+    def update_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
+        pass
+
+    def get_total_reimbursements_amount(self) -> float:
+        pass
+
+    def get_total_reimbursements_amount_by_employee(self) -> float:
+        pass
+
+    def get_total_reimbursements_amount_by_month(self) -> float:
+        pass
+
+    def get_total_reimbursements_amount_by_category(self) -> float:
+        pass
+
+    def get_top_five_highest_amounts(self) -> List[Reimbursement]:
+        pass
