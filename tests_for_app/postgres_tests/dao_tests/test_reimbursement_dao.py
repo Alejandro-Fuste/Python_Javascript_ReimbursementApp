468aba8937716b56266145301ef941b44dee0162
Alejandro F <afuste7@gmail.com>
2021-12-11 23:38:27 -0500

    added code for update reimbursement test

--- tests_for_app/postgres_tests/dao_tests/test_reimbursement_dao.py
+++ tests_for_app/postgres_tests/dao_tests/test_reimbursement_dao.py
@@ -3,10 +3,10 @@ from entities.reimbursement import Reimbursement
 
 reimbursement_dao = ReimbursementPostgresDAO()
 reimbursement: Reimbursement = Reimbursement(0, 20.00, 'Gas', 'Travel gas', '12-9-2021', 'pending', 'null',
-                                             'null', 'Rey Skywalker', 'Luke Skywalker')
+                                             'null', 'Rey', 'Skywalker', 'Luke', 'Skywalker')
 
 updated_reimbursement = Reimbursement(0, 100.00, 'Gas', 'Travel gas', '12-9-2021', 'pending', 'null',
-                                             'null', 'Rey Skywalker', 'Luke Skywalker')
+                                             'null', 'Rey', 'Skywalker', 'Luke', 'Skywalker')
 
 def test_create_new_reimbursement_request():
     create_reimbursement = reimbursement_dao.create_new_reimbursement_request(reimbursement)
@@ -22,7 +22,7 @@ def test_get_reimbursements_by_employee_id():
 
 def test_update_reimbursement_request():
     updated_request = reimbursement_dao.update_reimbursement_request(updated_reimbursement)
-    assert updated_request.employee_name ==
+    assert updated_request.employee_last_name == updated_reimbursement.employee_last_name
 
 def test_
 
