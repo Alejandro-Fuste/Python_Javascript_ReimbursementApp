5d4b6ac18673189b694be7dc11259a06e9fd117b
Alejandro F <afuste7@gmail.com>
2021-12-11 22:20:55 -0500

    added code for get employee by username test

--- tests_for_app/postgres_tests/dao_tests/test_employee_dao.py
+++ tests_for_app/postgres_tests/dao_tests/test_employee_dao.py
@@ -3,4 +3,9 @@ from entities.employee import Employee
 
 employee_dao = EmployeePostgresDAO()
 
+sample_user_name = 'Master Jedi'
+
+
 def test_get_employee_by_username():
+    user = employee_dao.get_employee_by_username(sample_user_name)
+    assert user.user_name == sample_user_name
