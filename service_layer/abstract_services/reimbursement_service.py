7d78bbb19b8eba47c0216be6f365d4eb932c3e57
Alejandro F <afuste7@gmail.com>
2021-12-12 19:29:19 -0500

    added code for reimbursement service abstract file

---
+++ service_layer/abstract_services/reimbursement_service.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from entities.reimbursement import Reimbursement
+
+
+class ReimbursementService(ABC):
+    # for employees to create new request
+    @abstractmethod
+    def service_create_new_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
+        pass
+
+    # so manager can view approved, denied, and pending reimbursements
+    @abstractmethod
+    def service_get_all_reimbursement_requests(self) -> List[Reimbursement]:
+        pass
+
+    # to view all the employees reimbursements
+    @abstractmethod
+    def service_get_reimbursements_by_employee_id(self, employee_id: int) -> List[Reimbursement]:
+        pass
+
+    # for manager to approve/deny and leave comments
+    @abstractmethod
+    def service_update_reimbursement_request(self, reimbursement: Reimbursement) -> Reimbursement:
+        pass
+
+    # get statistics
+    @abstractmethod
+    def service_get_total_reimbursements_amount(self) -> float:
+        pass
+
+    @abstractmethod
+    def service_get_total_reimbursements_amount_by_employee(self) -> float:
+        pass
+
+    @abstractmethod
+    def service_get_total_reimbursements_amount_by_month(self) -> float:
+        pass
+
+    @abstractmethod
+    def service_get_total_reimbursements_amount_by_category(self) -> float:
+        pass
+
+    @abstractmethod
+    def service_get_top_five_highest_amounts(self) -> List[Reimbursement]:
+        pass
