de260adc09502935dadd7eb85da6973a47bda4bf
Alejandro F <afuste7@gmail.com>
2021-12-13 12:22:47 -0500

    added code for get manager by username in dao

--- data_access_layer/implementation_classes/manager_postgres_dao_imp.py
+++ data_access_layer/implementation_classes/manager_postgres_dao_imp.py
@@ -5,4 +5,9 @@ from utils.database_connection import connection
 
 class ManagerPostgresDAO(ManagerDAO):
     def get_manager_by_username(self, user_name: str) -> Manager:
-        pass
+        sql = 'select * from "python_reimbursement".manager where user_name = %s'
+        cursor = connection.cursor()
+        cursor.execute(sql, [user_name])
+        manager_record = cursor.fetchone()
+        manager = Manager(*manager_record)
+        return manager
