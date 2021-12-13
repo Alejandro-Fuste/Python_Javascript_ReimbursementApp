4f4a5ef50c5c9639b6570460004aa2fd384272c8
Alejandro F <afuste7@gmail.com>
2021-12-12 21:35:27 -0500

    added employee not found exception

--- tests_for_app/postgres_tests/service_tests/test_reimbursement_service.py
+++ tests_for_app/postgres_tests/service_tests/test_reimbursement_service.py
@@ -1,3 +1,4 @@
+from custom_exceptions.duplicate_reimbursement_exception import DuplicateReimbursementException
 from data_access_layer.implementation_classes.reimbursement_postgres_dao_imp import ReimbursementPostgresDAO
 from entities.reimbursement import Reimbursement
 from service_layer.implementation_services.reimbursement_postgres_service import ReimbursementPostgresServiceImp
@@ -8,8 +9,10 @@ reimbursement_service = ReimbursementPostgresServiceImp(reimbursement_dao)
 duplicate_reimbursement = Reimbursement(1, 20.00, 'Gas', 'Travel gas', '12-9-2021', 'pending', 'null', 'null',
                                         'Rey', 'Skywalker', 'Luke', 'Skywalker')
 
+
 def test_catch_duplicate_reimbursement_request():
     try:
         reimbursement_service.service_create_new_reimbursement_request(duplicate_reimbursement)
         assert False
-    except
+    except DuplicateReimbursementException as e:
+        assert str(e) == "This reimbursement has already been created."
