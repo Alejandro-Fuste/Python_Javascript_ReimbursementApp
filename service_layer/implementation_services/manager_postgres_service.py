c0410794e9a06412ddd9cc0c407d7b85503e3a5a
Alejandro F <afuste7@gmail.com>
2021-12-12 20:43:28 -0500

    added code for manager service imp

---
+++ service_layer/implementation_services/manager_postgres_service.py
@@ -0,0 +1,11 @@
+from data_access_layer.implementation_classes.manager_postgres_dao_imp import ManagerPostgresDAO
+from entities.manager import Manager
+from service_layer.abstract_services.manager_service import ManagerService
+
+
+class ManagerPostgresServiceImp(ManagerService):
+    def __init__(self, manager_dao: ManagerPostgresDAO):
+        self.manager_dao = manager_dao
+
+    def service_get_manager_by_username(self, user_name: str) -> Manager:
+        pass
