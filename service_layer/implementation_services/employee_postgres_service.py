318806bf9f09bf69cc8c07a64bc67676b5527949
Alejandro F <afuste7@gmail.com>
2021-12-12 20:40:22 -0500

    added code for employee service imp

--- service_layer/implementation_services/employee_postgres_service.py
+++ service_layer/implementation_services/employee_postgres_service.py
@@ -2,3 +2,11 @@ from custom_exceptions.invalid_credentials_exception import InvalidCredentialsEx
 from data_access_layer.implementation_classes.employee_postgres_dao_imp import EmployeePostgresDAO
 from entities.employee import Employee
 from service_layer.abstract_services.employee_service import EmployeeService
+
+
+class EmployeePostgresServiceImp(EmployeeService):
+    def __init__(self, employee_dao: EmployeePostgresDAO):
+        self.employee_dao = employee_dao
+
+    def service_get_employee_by_username(self, user_name: str) -> Employee:
+        pass
