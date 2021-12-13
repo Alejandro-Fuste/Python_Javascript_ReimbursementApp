64abee05385ca547bc8b1c8c3424bd4315437e53
Alejandro F <afuste7@gmail.com>
2021-12-12 20:58:42 -0500

    added test for employee service

--- tests_for_app/postgres_tests/service_tests/test_employee_service.py
+++ tests_for_app/postgres_tests/service_tests/test_employee_service.py
@@ -1,4 +1,17 @@
 from custom_exceptions.invalid_credentials_exception import InvalidCredentialsException
 from data_access_layer.implementation_classes.employee_postgres_dao_imp import EmployeePostgresDAO
-from entities.employee import Employee
-from service_layer.implementation_services.employee_postgres_service import EmployeePostgresService
+
+from service_layer.implementation_services.employee_postgres_service import EmployeePostgresServiceImp
+
+employee_dao = EmployeePostgresDAO()
+employee_service = EmployeePostgresServiceImp(employee_dao)
+
+non_employee = 'anakin_skywalker'
+
+
+def test_employee_not_found_for_get_employee_by_username():
+    try:
+        employee_service.service_get_employee_by_username(non_employee)
+        assert False
+    except InvalidCredentialsException as e:
+        assert str(e) == "The provide credentials are invalid."
