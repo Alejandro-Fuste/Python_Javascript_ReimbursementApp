c5a675477ef525d7a6fb762179735a6b20d698df
Alejandro F <afuste7@gmail.com>
2021-12-14 12:54:59 -0500

    added code for get manager by username service

--- service_layer/implementation_services/manager_postgres_service.py
+++ service_layer/implementation_services/manager_postgres_service.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from custom_exceptions.invalid_credentials_exception import InvalidCredentialsException
 from data_access_layer.implementation_classes.manager_postgres_dao_imp import ManagerPostgresDAO
 from entities.manager import Manager
 from service_layer.abstract_services.manager_service import ManagerService
@@ -10,7 +11,11 @@ class ManagerPostgresServiceImp(ManagerService):
         self.manager_dao = manager_dao
 
     def service_get_manager_by_username(self, user_name: str) -> Manager:
-        pass
+        managers = self.manager_dao.get_all_managers()
+        for current_manager in managers:
+            if current_manager.user_name == user_name:
+                return self.manager_dao.get_manager_by_username(user_name)
+            raise InvalidCredentialsException("The provided credentials are invalid.")
 
     def get_all_managers(self) -> List[Manager]:
         return self.manager_dao.get_all_managers()
