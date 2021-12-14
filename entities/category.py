2c3b7b86366e04b9ba6d25848f0cb0b66b8e75ae
Alejandro F <afuste7@gmail.com>
2021-12-14 17:49:21 -0500

    added property to category entity

--- entities/category.py
+++ entities/category.py
@@ -1,12 +1,14 @@
 class Category:
-    def __init__(self, category_name: str):
+    def __init__(self, category_id: int, category_name: str):
+        self.category_id = category_id
         self.category_name = category_name
 
     def __str__(self):
-        return f'category name: {self.category_name}'
+        return f'category id: {self.category_id}, category name: {self.category_name}'
 
     def make_category_dictionary(self):
         return {
+            "categoryId": self.category_id,
             "categoryName": self.category_name
         }
 
