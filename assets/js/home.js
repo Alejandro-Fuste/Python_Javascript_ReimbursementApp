58f2d35681054833421854abce99a5d8d0b46c69
Alejandro F <afuste7@gmail.com>
2021-12-15 22:59:18 -0500

    added id for form and added event listener for form

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,9 @@
+const formEl = document.querySelector("#loginForm");
 
+function validateLogin(e) {
+  e.preventDefault();
+
+  console.log("Form was submitted");
+}
+
+formEl.addEventListener("submit", validateLogin);
