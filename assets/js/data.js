30fee9d59c0ad12ab70e256079a338e65aee7780
Alejandro F <afuste7@gmail.com>
2021-12-19 15:37:31 -0500

    added function to display each reimbursement to employee page

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,14 +1,62 @@
-const getData = async (url) => {
-  const response = await fetch(url);
+// const getData = async (url) => {
+//   const response = await fetch(url);
 
-  let data = await response.json();
+//   let data = await response.json();
 
-  return data;
-};
+//   return data;
+// };
 
 const getName = () => {
   let tokenData = getToken();
   return `${tokenData["firstName"]} ${tokenData["lastName"]}`;
+};
+
+const addReimbursements = (data) => {
+    const selectEl = document.querySelector('#reimbUl');
+
+    data.forEach((c) => {
+        let createLiTag = document.createElement('li');
+        createLiTag.setAttribute('id', 'reimLi');
+
+        let createDivTag = document.createElement('div');
+        createDivTag.setAttribute('id', 'reimDiv');
+
+        let createIdTag = document.createElement('p');
+        createIdTag.setAttribute('id', 'reimP');
+        createIdTag.textContent = c.reimbursementId;
+
+        let createStatusTag = document.createElement('p');
+        createStatusTag.setAttribute('id', 'reimP');
+        createStatusTag.textContent = c.status;
+
+        let createDateTag = document.createElement('p');
+        createDateTag.setAttribute('id', 'reimP');
+        createDateTag.textContent = c.reimbursementDate;
+
+        let createCategoryTag = document.createElement('p');
+        createCategoryTag.setAttribute('id', 'reimP');
+        createCategoryTag.textContent = c.category;
+
+        let createAmountTag = document.createElement('p');
+        createAmountTag.setAttribute('id', 'reimP');
+        createAmountTag.textContent = c.reimbursementAmount;
+
+        let createReimbursementReasonTag = document.createElement('p');
+        createReimbursementReasonTag.setAttribute('id', 'reimP');
+        createReimbursementReasonTag.textContent = c.reimbursementReason;
+
+        let createDecisionDateTag = document.createElement('p');
+        createDecisionDateTag.setAttribute('id', 'reimP');
+        createDecisionDateTag.textContent = c.decisionDate;
+
+        let createReasonTag = document.createElement('p');
+        createReasonTag.setAttribute('id', 'reimP');
+        createReasonTag.textContent = c.reason;
+
+        createDivTag.append(createIdTag, createStatusTag, createDateTag, createCategoryTag, createAmountTag, createReimbursementReasonTag, createDecisionDateTag, createReasonTag);
+        createLiTag.appendChild(createDivTag);
+        selectEl.appendChild(createLiTag);
+    })
 };
 
 const addCategories = (data) => {
@@ -19,7 +67,7 @@ const addCategories = (data) => {
         createTag.textContent = c.categoryName;
         selectEl.appendChild(createTag);
     })
-}
+};
 
 const addManagers = (data) => {
     const selectEl = document.querySelector('#validationCustom05');
@@ -30,7 +78,7 @@ const addManagers = (data) => {
         createTag.textContent = `${c['firstName']} ${c['lastName']}`;
         selectEl.appendChild(createTag);
     })
-}
+};
 
 // validate values for reimbursement
 const validateUserInput = () => {
@@ -68,7 +116,7 @@ const createObject = () => {
     object["employeeId"] = JSON.parse(localStorage.getItem('pseudoToken')).employeeId;
     
    return object
-}
+};
 
 // send data to create reimbursement
 const createReimbursement = async (url,data) => {
@@ -90,4 +138,4 @@ const createReimbursement = async (url,data) => {
         alert(content.message);
     
       }
-}
+};
--- assets/js/employee.js
+++ assets/js/employee.js
@@ -2,9 +2,9 @@
 window.addEventListener('load', verifyToken('Manager'));
 
 // Logout and employee name selectors
-const logoutEl = document.querySelector("#logoutEl");
+const logoutButtonEl = document.querySelector("#logoutEl");
 const nameEl = document.querySelector('#employeeName');
-const formEl = document.querySelector('#reimbursementForm');
+const reimbursementFormEl = document.querySelector('#reimbursementForm');
 
 // Add name to element
 nameEl.textContent = getName();
@@ -13,22 +13,23 @@ nameEl.textContent = getName();
 // Get id from local storage and add it to url for get all employee reimbursements
 let tokenData = getToken();
 let id = tokenData['employeeId']
-let url = `http://127.0.0.1:5000/reimbursements/${id}`;
-getData(url);
+let reimbursementsUrl = `http://127.0.0.1:5000/reimbursements/${id}`;
+// getData(url);
+fetch(reimbursementsUrl).then(res => res.json()).then(data => addReimbursements(data)).catch(err => console.log(err));
 
 // Get all categories 
 let categoryUrl = 'http://127.0.0.1:5000/categories';
-fetch(categoryUrl).then(res => res.json()).then(data => addCategories(data));
+fetch(categoryUrl).then(res => res.json()).then(data => addCategories(data)).catch(err => console.log(err));
 
 
 // Get all managers
 let managersUrl = 'http://127.0.0.1:5000/managers';
-fetch(managersUrl).then(res => res.json()).then(data => addManagers(data));
+fetch(managersUrl).then(res => res.json()).then(data => addManagers(data)).catch(err => console.log(err));
 
 
 // Logout event listener
-logoutEl.addEventListener("click", logout);
-formEl.addEventListener("submit", (e) =>{
+logoutButtonEl.addEventListener("click", logout);
+reimbursementFormEl.addEventListener("submit", (e) =>{
     e.preventDefault();
 
     let reimbursementUrl = 'http://127.0.0.1:5000/reimbursement';
