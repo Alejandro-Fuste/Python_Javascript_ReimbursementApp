a1d9440e83d9e968a4f74cdda2b831d7e91f3c14
Alejandro F <afuste7@gmail.com>
2021-12-19 21:38:16 -0500

    added code to get total reimbursement by employee

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,6 +5,8 @@
 
 //   return data;
 // };
+const errorAlert = () =>
+  alert("Opps, something is wrong with the server connection");
 
 const getName = () => {
   let tokenData = getToken();
@@ -12,132 +14,149 @@ const getName = () => {
 };
 
 const addReimbursements = (data) => {
-    const selectEl = document.querySelector('#reimbUl');
+  const selectEl = document.querySelector("#reimbUl");
 
-    data.forEach((c) => {
-        let createLiTag = document.createElement('li');
-        createLiTag.setAttribute('id', 'reimLi');
+  data.forEach((c) => {
+    let createLiTag = document.createElement("li");
+    createLiTag.setAttribute("id", "reimLi");
 
-        let createDivTag = document.createElement('div');
-        createDivTag.setAttribute('id', 'reimDiv');
+    let createDivTag = document.createElement("div");
+    createDivTag.setAttribute("id", "reimDiv");
 
-        let createIdTag = document.createElement('p');
-        createIdTag.setAttribute('id', 'reimP');
-        createIdTag.textContent = `Id: ${c.reimbursementId}`;
+    let createIdTag = document.createElement("p");
+    createIdTag.setAttribute("id", "reimP");
+    createIdTag.textContent = `Id: ${c.reimbursementId}`;
 
-        let createStatusTag = document.createElement('p');
-        createStatusTag.setAttribute('id', 'reimP');
-        createStatusTag.textContent = `Status: ${c.status}`;
+    let createStatusTag = document.createElement("p");
+    createStatusTag.setAttribute("id", "reimP");
+    createStatusTag.textContent = `Status: ${c.status}`;
 
-        let createDateTag = document.createElement('p');
-        createDateTag.setAttribute('id', 'reimP');
-        createDateTag.textContent = `Created Date: ${c.reimbursementDate}`;
+    let createDateTag = document.createElement("p");
+    createDateTag.setAttribute("id", "reimP");
+    createDateTag.textContent = `Created Date: ${c.reimbursementDate}`;
 
-        let createCategoryTag = document.createElement('p');
-        createCategoryTag.setAttribute('id', 'reimP');
-        createCategoryTag.textContent = `Category: ${c.category}`;
+    let createCategoryTag = document.createElement("p");
+    createCategoryTag.setAttribute("id", "reimP");
+    createCategoryTag.textContent = `Category: ${c.category}`;
 
-        let createAmountTag = document.createElement('p');
-        createAmountTag.setAttribute('id', 'reimP');
-        createAmountTag.textContent = `Amount: ${c.reimbursementAmount}`;
+    let createAmountTag = document.createElement("p");
+    createAmountTag.setAttribute("id", "reimP");
+    createAmountTag.textContent = `Amount: ${c.reimbursementAmount}`;
 
-        let createReimbursementReasonTag = document.createElement('p');
-        createReimbursementReasonTag.setAttribute('id', 'reimP');
-        createReimbursementReasonTag.textContent = `Reason for reimbursement: ${c.reimbursementReason}`;
+    let createReimbursementReasonTag = document.createElement("p");
+    createReimbursementReasonTag.setAttribute("id", "reimP");
+    createReimbursementReasonTag.textContent = `Reason for reimbursement: ${c.reimbursementReason}`;
 
-        let createDecisionDateTag = document.createElement('p');
-        createDecisionDateTag.setAttribute('id', 'reimP');
-        createDecisionDateTag.textContent = `Decision Date: ${c.decisionDate}`;
+    let createDecisionDateTag = document.createElement("p");
+    createDecisionDateTag.setAttribute("id", "reimP");
+    createDecisionDateTag.textContent = `Decision Date: ${c.decisionDate}`;
 
-        let createReasonTag = document.createElement('p');
-        createReasonTag.setAttribute('id', 'reimP');
-        createReasonTag.textContent = `Manager comment: ${c.reason}`;
+    let createReasonTag = document.createElement("p");
+    createReasonTag.setAttribute("id", "reimP");
+    createReasonTag.textContent = `Manager comment: ${c.reason}`;
 
-        createDivTag.append(createIdTag, createStatusTag, createDateTag, createCategoryTag, createAmountTag, createReimbursementReasonTag, createDecisionDateTag, createReasonTag);
-        createLiTag.appendChild(createDivTag);
-        selectEl.appendChild(createLiTag);
-    })
+    createDivTag.append(
+      createIdTag,
+      createStatusTag,
+      createDateTag,
+      createCategoryTag,
+      createAmountTag,
+      createReimbursementReasonTag,
+      createDecisionDateTag,
+      createReasonTag
+    );
+    createLiTag.appendChild(createDivTag);
+    selectEl.appendChild(createLiTag);
+  });
 };
 
 const addCategories = (data) => {
-    const selectEl = document.querySelector('#validationCustom04');
-     
-    data.forEach((c) => {
-        let createTag = document.createElement('option');
-        createTag.textContent = c.categoryName;
-        selectEl.appendChild(createTag);
-    })
+  const selectEl = document.querySelector("#validationCustom04");
+
+  data.forEach((c) => {
+    let createTag = document.createElement("option");
+    createTag.textContent = c.categoryName;
+    selectEl.appendChild(createTag);
+  });
 };
 
 const addManagers = (data) => {
-    const selectEl = document.querySelector('#validationCustom05');
-    
-    data.forEach((c) => {
-        let createTag = document.createElement('option');
-        createTag.setAttribute('data-id', c.managerId);
-        createTag.textContent = `${c['firstName']} ${c['lastName']}`;
-        selectEl.appendChild(createTag);
-    })
+  const selectEl = document.querySelector("#validationCustom05");
+
+  data.forEach((c) => {
+    let createTag = document.createElement("option");
+    createTag.setAttribute("data-id", c.managerId);
+    createTag.textContent = `${c["firstName"]} ${c["lastName"]}`;
+    selectEl.appendChild(createTag);
+  });
 };
 
 const addReimbursementsManager = (data) => {
-    const selectEl = document.querySelector('#reimbUL');
+  const selectEl = document.querySelector("#reimbUL");
 
-    data.forEach((c) => {
-        let buttonDiv = document.createElement('div');
+  data.forEach((c) => {
+    let buttonDiv = document.createElement("div");
 
-        let createLiTag = document.createElement('li');
-        createLiTag.setAttribute('id', c.status);
+    let createLiTag = document.createElement("li");
+    createLiTag.setAttribute("id", c.status);
 
-        let createDivTag = document.createElement('div');
-        createDivTag.setAttribute('id', 'reimDiv');
+    let createDivTag = document.createElement("div");
+    createDivTag.setAttribute("id", "reimDiv");
 
-        let createIdTag = document.createElement('p');
-        createIdTag.setAttribute('id', 'reimP');
-        createIdTag.textContent = `Id: ${c.reimbursementId}`;
+    let createIdTag = document.createElement("p");
+    createIdTag.setAttribute("id", "reimP");
+    createIdTag.textContent = `Id: ${c.reimbursementId}`;
 
-        if (c.status === 'pending') {
-            let createButton = document.createElement('button');
-            createButton.setAttribute('id', 'pendingButton');
-            createButton.textContent = '+';
-            buttonDiv.append(createIdTag, createButton);
-            createDivTag.append(buttonDiv);
-        } else {
-            createDivTag.append(createIdTag)
-        }
+    if (c.status === "pending") {
+      let createButton = document.createElement("button");
+      createButton.setAttribute("id", "pendingButton");
+      createButton.textContent = "+";
+      buttonDiv.append(createIdTag, createButton);
+      createDivTag.append(buttonDiv);
+    } else {
+      createDivTag.append(createIdTag);
+    }
 
-        let createStatusTag = document.createElement('p');
-        createStatusTag.setAttribute('id', 'reimP');
-        createStatusTag.textContent = `Status: ${c.status}`;
+    let createStatusTag = document.createElement("p");
+    createStatusTag.setAttribute("id", "reimP");
+    createStatusTag.textContent = `Status: ${c.status}`;
 
-        let createDateTag = document.createElement('p');
-        createDateTag.setAttribute('id', 'reimP');
-        createDateTag.textContent = `Created Date: ${c.reimbursementDate}`;
+    let createDateTag = document.createElement("p");
+    createDateTag.setAttribute("id", "reimP");
+    createDateTag.textContent = `Created Date: ${c.reimbursementDate}`;
 
-        let createCategoryTag = document.createElement('p');
-        createCategoryTag.setAttribute('id', 'reimP');
-        createCategoryTag.textContent = `Category: ${c.category}`;
+    let createCategoryTag = document.createElement("p");
+    createCategoryTag.setAttribute("id", "reimP");
+    createCategoryTag.textContent = `Category: ${c.category}`;
 
-        let createAmountTag = document.createElement('p');
-        createAmountTag.setAttribute('id', 'reimP');
-        createAmountTag.textContent = `Amount: ${c.reimbursementAmount}`;
+    let createAmountTag = document.createElement("p");
+    createAmountTag.setAttribute("id", "reimP");
+    createAmountTag.textContent = `Amount: ${c.reimbursementAmount}`;
 
-        let createReimbursementReasonTag = document.createElement('p');
-        createReimbursementReasonTag.setAttribute('id', 'reimP');
-        createReimbursementReasonTag.textContent = `Reason for reimbursement: ${c.reimbursementReason}`;
+    let createReimbursementReasonTag = document.createElement("p");
+    createReimbursementReasonTag.setAttribute("id", "reimP");
+    createReimbursementReasonTag.textContent = `Reason for reimbursement: ${c.reimbursementReason}`;
 
-        let createDecisionDateTag = document.createElement('p');
-        createDecisionDateTag.setAttribute('id', 'reimP');
-        createDecisionDateTag.textContent = `Decision Date: ${c.decisionDate}`;
+    let createDecisionDateTag = document.createElement("p");
+    createDecisionDateTag.setAttribute("id", "reimP");
+    createDecisionDateTag.textContent = `Decision Date: ${c.decisionDate}`;
 
-        let createReasonTag = document.createElement('p');
-        createReasonTag.setAttribute('id', 'reimP');
-        createReasonTag.textContent = `Manager comment: ${c.reason}`;
+    let createReasonTag = document.createElement("p");
+    createReasonTag.setAttribute("id", "reimP");
+    createReasonTag.textContent = `Manager comment: ${c.reason}`;
 
-        createDivTag.append(createStatusTag, createDateTag, createCategoryTag, createAmountTag, createReimbursementReasonTag, createDecisionDateTag, createReasonTag);
-        createLiTag.appendChild(createDivTag);
-        selectEl.appendChild(createLiTag);
-    })
+    createDivTag.append(
+      createStatusTag,
+      createDateTag,
+      createCategoryTag,
+      createAmountTag,
+      createReimbursementReasonTag,
+      createDecisionDateTag,
+      createReasonTag
+    );
+    createLiTag.appendChild(createDivTag);
+    selectEl.appendChild(createLiTag);
+  });
 };
 
 // validate values for reimbursement
@@ -150,91 +169,108 @@ const validateUserInput = () => {
   validate.forEach((c) => {
     if (c.value === "" || c.value <= 0) {
       c.style.borderColor = "red";
-      c.nextElementSibling.style.display = 'block';
-    } else if (c.dataset.name === 'managerId') {
-        validated[c.dataset.name] = parseInt(document.querySelector('[data-id]').dataset.id);
+      c.nextElementSibling.style.display = "block";
+    } else if (c.dataset.name === "managerId") {
+      validated[c.dataset.name] = parseInt(
+        document.querySelector("[data-id]").dataset.id
+      );
     } else if (c.value > 0) {
-        validated[c.dataset.name] = parseFloat(c.value);
+      validated[c.dataset.name] = parseFloat(c.value);
     } else {
-        validated[c.dataset.name] = c.value.trim();
+      validated[c.dataset.name] = c.value.trim();
     }
   });
-  
+
   return validated;
 };
 
 // object holding data for creating reimbursement
 const createObject = () => {
-    //values user entered into form
-   let object = validateUserInput();
+  //values user entered into form
+  let object = validateUserInput();
 
-    // the rest of the values needed to create new reimbursment
-    object["reimbursementId"] = 0;
-    object["status"] = "pending";
-    object["decisionDate"] = "null";
-    object["reason"] = "null";
-    object["employeeId"] = JSON.parse(localStorage.getItem('pseudoToken')).employeeId;
-    
-   return object
+  // the rest of the values needed to create new reimbursment
+  object["reimbursementId"] = 0;
+  object["status"] = "pending";
+  object["decisionDate"] = "null";
+  object["reason"] = "null";
+  object["employeeId"] = JSON.parse(
+    localStorage.getItem("pseudoToken")
+  ).employeeId;
+
+  return object;
 };
 
 // send data to create reimbursement
-const createReimbursement = async (url,data) => {
-    const response = await fetch(url, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-    
-      if (response.status === 201) {
-        let content = await response.json();
-        alert('Your reimbursement was created.');
-      } else {
-        let content = await response.json();
-        alert(content.message);
-    
-      }
+const createReimbursement = async (url, data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.status === 201) {
+    let content = await response.json();
+    alert("Your reimbursement was created.");
+  } else {
+    let content = await response.json();
+    alert(content.message);
+  }
 };
 
 const allButtonDisplay = () => {
-    let liElements = document.querySelectorAll("li");
-    let liArray = [...liElements];
+  let liElements = document.querySelectorAll("li");
+  let liArray = [...liElements];
 
-    liArray.forEach(c => c.style.display = 'block');
-    
+  liArray.forEach((c) => (c.style.display = "block"));
 };
 
 const approvedButtonDisplay = () => {
-    allButtonDisplay();
-    let liElements = document.querySelectorAll("li");
-    let liArray = [...liElements];
-    let filtered = liArray.filter(c => c.id != 'approved');
+  allButtonDisplay();
+  let liElements = document.querySelectorAll("li");
+  let liArray = [...liElements];
+  let filtered = liArray.filter((c) => c.id != "approved");
 
-    filtered.forEach(c => c.style.display = 'none');
-    
+  filtered.forEach((c) => (c.style.display = "none"));
 };
 
-
 const deniendButtonDisplay = () => {
-    allButtonDisplay();
-    let liElements = document.querySelectorAll("li");
-    let liArray = [...liElements];
-    let filtered = liArray.filter(c => c.id != 'rejected');
+  allButtonDisplay();
+  let liElements = document.querySelectorAll("li");
+  let liArray = [...liElements];
+  let filtered = liArray.filter((c) => c.id != "rejected");
 
-    filtered.forEach(c => c.style.display = 'none');
-    
+  filtered.forEach((c) => (c.style.display = "none"));
 };
 
 const pendingButtonDisplay = () => {
-    allButtonDisplay();
-    let liElements = document.querySelectorAll("li");
-    let liArray = [...liElements];
-    let filtered = liArray.filter(c => c.id != 'pending');
+  allButtonDisplay();
+  let liElements = document.querySelectorAll("li");
+  let liArray = [...liElements];
+  let filtered = liArray.filter((c) => c.id != "pending");
 
-    filtered.forEach(c => c.style.display = 'none');
-    
+  filtered.forEach((c) => (c.style.display = "none"));
+};
+
+const displayTotalReimbursements = (data) => {
+  let totalEl = document.querySelector("#totalAppReimb");
+  totalEl.textContent = data.total;
+};
+
+const displayTotalReimbursementByEmployee = (data) => {
+  const empEl = document.querySelector("#employeeSelect").value;
+
+  const totalEmpUrl = `http://127.0.0.1:5000/reimbursements/total/${empEl}`;
+  
+  fetch(totalEmpUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let totalEl = document.querySelector("#totalEmpReimb");
+      totalEl.textContent = data.total;
+    })
+    .catch((err) => errorAlert);
 };
--- assets/js/manager.js
+++ assets/js/manager.js
@@ -8,14 +8,18 @@ const allEl = document.querySelector('#allButton');
 const approvedEl = document.querySelector('#approvedButton');
 const deniedEl = document.querySelector('#deniedButton');
 const pendingEl = document.querySelector('#pendingButton');
+const empEl = document.querySelector("#employeeSelect");
 
 // Add name to element
 nameEl.textContent = getName();
 
-
 // Get all reimbursements 
 const url = `http://127.0.0.1:5000/reimbursements/all`
-fetch(url).then(res => res.json()).then(data => addReimbursementsManager(data)).catch(err => alert("Opps, something is wrong with the server connection"));
+fetch(url).then(res => res.json()).then(data => addReimbursementsManager(data)).catch(err => errorAlert);
+
+// Get total reimbursement
+const totalUrl = `http://127.0.0.1:5000/reimbursements/total`
+fetch(totalUrl).then(res => res.json()).then(data => displayTotalReimbursements(data)).catch(err => errorAlert)
 
 // All button event listener
 allEl.addEventListener('click', allButtonDisplay);
@@ -31,3 +35,6 @@ pendingEl.addEventListener('click', pendingButtonDisplay);
 
 // Logout button event listener
 logoutEl.addEventListener("click", logout);
+
+// Get total reimbursement by employee
+empEl.addEventListener('change', displayTotalReimbursementByEmployee);
